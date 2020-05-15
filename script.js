a946eb1d1eb574b8933eb1816e3c28b4422a0f6e
randominternetguy <neillxander@gmail.com>
2020-05-15 15:15:35 +0530

    Made the page responsive and added a button to change grid size.

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const container = document.querySelector("#grid-container");
+const body = document.querySelector("body");
+const gridSizeButton = document.querySelector("#change-grid");
 let gridSize = 16;
-
-container.style.cssText = `
-  display: grid;
-  grid-template-columns: repeat(${gridSize}, 1fr);
-  grid-template-rows: repeat(${gridSize}, 1fr);
-`
 
 // return random RGB values.
 let setRandomRGB = () => {
@@ -17,20 +12,47 @@ let setRandomRGB = () => {
   return rgbvalues.toString();
 }
 
-// create a {gridSize}x{gridSize} grid.
+// creates a {gridSize}x{gridSize} grid.
 let createGrid = (size) => {
+  // if there is a grid container remove it.
+  if (document.querySelector("#grid-container")){
+    body.removeChild(document.querySelector("#grid-container"));
+  }
+
+  const container = document.createElement('div');
+  container.setAttribute('id', 'grid-container');
+
+  container.style.cssText = `
+  display: grid;
+  grid-template-columns: repeat(${size}, 1fr);
+  grid-template-rows: repeat(${size}, 1fr);
+  `
+
   for (let i = 0; i <= size - 1; i++){
     for (let j = 0; j <= size - 1; j++){
       let grid = document.createElement('div');
       grid.classList.add('grid');
       grid.addEventListener("mouseover", () => {
+        // background color changes to random color.
         grid.style.cssText = `
-        background-color: rgb(${setRandomRGB()});
+        background-color: rgb(${setRandomRGB()}) ;
       `
       });
       container.appendChild(grid);
     }
   }
+  body.appendChild(container);
 }
 
 createGrid(gridSize);
+
+// change the grid size when user clicks the gridSizeButton.
+gridSizeButton.addEventListener('click', () => {
+  gridSize = prompt('How many grids do you want the box to have?\n(Max size = 50)');
+  if (gridSize <= 50){
+    createGrid(gridSize);
+  }
+  else {
+    alert('Please enter a number below 50');
+  }
+})
